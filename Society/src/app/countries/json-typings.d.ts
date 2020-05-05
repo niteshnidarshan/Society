018dfbe6b23899f967bb934fc2d3fe67ab71acdc
Nitesh Nidarshan <niteshfromkatihar@gmail.com>
2020-05-05 19:27:01 +0530

    structure chages

--- Society/src/app/countries/json-typings.d.ts
+++
@@ -1,9 +0,0 @@
-/***
- * 
- * This file is recomened to read json file in countries.component.ts
- * This file must be inside countries component
- */
-declare module "*.json" {
-    const value: any;
-    export default value;
-    }
